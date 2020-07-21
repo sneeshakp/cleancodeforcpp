4a8980fc36f7da0a9e1d46d8465f0c5e13999d70
sneeshakp <68599657+sneeshakp@users.noreply.github.com>
2020-07-21 19:44:06 +0530

    Update paramchecker.cpp
    Commit to remove
    1)unused variables
    2)CCN

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,29 @@
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  int i;
+  
+bool bpm_check(float bpm)
+{
   if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
-  }
+    return false;}
+  
   return true;
+    
+}
+
+bool spo2_check(float spo2)
+{
+  if(spo2 < 80) {
+    return false;}
+  return true;
+}
+
+bool respRate_check(float respRate)
+{
+  if(respRate < 30 || respRate > 60) {
+    return false;}
+  return true;
+}
+
+bool vitalsAreOk(float bpm, float spo2, float respRate) 
+{
+bool ret = bpm_check(bpm)  && spo2_check(spo2) && respRate_check(respRate);
+  return ret;
 }
